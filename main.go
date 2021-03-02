ae758a09694a2c61347b7e16ed6d8d4f6c2e0947
Carlos Cardoso <cadu@oscardoso.com.br>
2021-03-02 00:44:27 -0500

    Workaround for HomeKit protocol issues

--- main.go
+++ main.go
@@ -22,10 +22,11 @@ func main() {
 	device = NewDevice(config.Device.Address)
 	acc = NewGarageDoor(config.Accessory.SerialNumber, AppVersion)
 
-	determineStatus()
-	acc.TargetDoorState.OnValueRemoteUpdate(commandToTransition)
+	acc.TargetDoorState.OnValueRemoteUpdate(targetTransitionCommand)
 	registerSensorForMonitoring(device.sensorOpen)
 	registerSensorForMonitoring(device.sensorClosed)
+
+	determineStatus()
 
 	startHomeKitServer()
 }
@@ -39,10 +40,11 @@ func registerSensorForMonitoring(sensor gpio.PinIn) {
 	}()
 }
 
-func commandToTransition(value int) {
-	// transition to state
+func targetTransitionCommand(newState int) {
 	device.Click(config.Device.Channel)
-	if value == characteristic.TargetDoorStateOpen {
+
+	// indicates we're transitioning...
+	if newState == characteristic.TargetDoorStateOpen {
 		acc.CurrentDoorState.UpdateValue(characteristic.CurrentDoorStateOpening)
 	} else {
 		acc.CurrentDoorState.UpdateValue(characteristic.CurrentDoorStateClosing)
@@ -57,31 +59,75 @@ func determineStatus() {
 	isClosed := device.sensorClosedStatus()
 
 	if isOpen && !isClosed {
-		acc.CurrentDoorState.UpdateValue(characteristic.CurrentDoorStateOpen)
-		acc.TargetDoorState.UpdateValue(characteristic.TargetDoorStateOpen)
+		log.Println("determineStatus - isOpen")
+		updateState(characteristic.CurrentDoorStateOpen)
 
 	} else if !isOpen && isClosed {
-		acc.CurrentDoorState.UpdateValue(characteristic.CurrentDoorStateClosed)
-		acc.TargetDoorState.UpdateValue(characteristic.TargetDoorStateClosed)
+		log.Println("determineStatus - isClosed")
+		updateState(characteristic.CurrentDoorStateClosed)
 
 	} else if !isOpen && !isClosed {
+		log.Println("determineStatus - not fully closed or open")
 		currentDoorState := acc.CurrentDoorState.GetValue()
-		if currentDoorState != characteristic.CurrentDoorStateOpen &&
-			currentDoorState != characteristic.CurrentDoorStateClosed {
-			// if we're in the middle of something, let it finish.
+		targetDoorState := acc.TargetDoorState.GetValue()
+
+		log.Println("determineStatus - current", currentDoorState, "target", targetDoorState)
+
+		switch currentDoorState {
+		case characteristic.CurrentDoorStateOpening, characteristic.CurrentDoorStateClosing:
+			// TODO: Figure out what to test here to ensure we're in the middle
+			// of something and not just lost. add a timer maybe to validate for
+			// how long we're in the same moving state before we decide to
+			// notify the user.
 			return
 		}
 
-		targetDoorState := acc.TargetDoorState.GetValue()
 		if targetDoorState == characteristic.TargetDoorStateClosed {
+			log.Println("determineStatus - setting target state to open")
 			acc.TargetDoorState.UpdateValue(characteristic.TargetDoorStateOpen)
 		} else {
+			log.Println("determineStatus - setting target state to closed")
 			acc.TargetDoorState.UpdateValue(characteristic.TargetDoorStateClosed)
 		}
 
 	} else {
+		log.Println("determineStatus - who knows?!?")
 		// TODO: This means that both sensors are connected
 		// FIXME: Figure out what to do in this situation!!
+	}
+}
+
+func updateState(toState int) {
+	oldState := acc.CurrentDoorState.GetValue()
+
+	// TODO: should we worry about this scenario?
+	// if oldState == toState {
+	// 	log.Println("updateState - same state, leaving...")
+	// 	return
+	// }
+
+	log.Println("updateState - switchting from ", oldState, "to", toState)
+	switch toState {
+	case characteristic.CurrentDoorStateClosing,
+		characteristic.CurrentDoorStateOpening,
+		characteristic.CurrentDoorStateStopped:
+
+		log.Println("updateState - [ 2, 3, 4 ]:", toState)
+		acc.CurrentDoorState.UpdateValue(toState)
+
+	case characteristic.CurrentDoorStateOpen,
+		characteristic.CurrentDoorStateClosed:
+
+		log.Println("updateState - [ 0, 1 ]:", toState)
+		// Make sure we only update target status if CurrentDoorState is Stopped.
+		if oldState != characteristic.CurrentDoorStateStopped {
+			log.Println("upateStatus updates targetdoorstate")
+			acc.TargetDoorState.UpdateValue(toState)
+		}
+		// Also, make sure to update TargetDoorState before CurrentDoorState. There
+		// are some funky situations where updating TargetDoorState after
+		// CurrentDoorState causes a new transition to happen.
+		acc.CurrentDoorState.UpdateValue(toState)
 	}
 }
 
